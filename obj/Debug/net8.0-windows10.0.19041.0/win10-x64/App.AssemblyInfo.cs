591c4e7759c13e069fcf02ec8259149f687a0443
yosss <juliusjrclastimosa@gmail.com>
2025-03-19 12:01:04 +0800

    improve login ui

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/App.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/App.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0cff4d1aa9b9976d95fed494af6d82b976322b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3c291ca0fc20a71556253a6677239a229e368c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
